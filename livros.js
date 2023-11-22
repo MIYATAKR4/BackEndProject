da85d235dcaa3d869c142380b5fcfd751bc343a9
MIYATAKR4 <catarina.cmd@gmail.com>
2023-11-22 13:22:54 -0300

    chore: post function

--- livros.js
+++ livros.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const { v4: uuidv4 } = require('uuid'); // Importando o uuidv4 para gerar um id aleatório
+const express = require('express'); // Importando o express
+const router = express.Router(); // Importando o router do express
 
-const app = express();
-const porta = 3333;
+const app = express(); // Iniciando o express
+app.use(express.json()); // Usando o express para trabalhar com json
+const porta = 3333; // Definindo a porta padrão
 
+// Criando uma lista de livros
 const livros = [
     {
+        id: '1',
         nome: "Retalhos ",
         imagem: "https://i.postimg.cc/NMf5W3Qf/image.jpg",
         descricao: "Thompson retrata sua própria história, da infância até o início da vida adulta, numa cidadezinha de Wisconsin, no centro dos Estados Unidos, que parece estar sempre coberta pela neve.",
@@ -14,6 +18,7 @@ const livros = [
     },
 
     {
+        id: '2',
         nome: "As Muitas Mortes de Laila Starr",
         imagem: "https://i.postimg.cc/tCz1qd4M/717-Fw89-LFs-L.jpg",
         descricao: "Até a Morte está desempregada. Oi? Com a humanidade prestes a descobrir o segredo da imortalidade, o avatar da Morte é demitido e relegado ao mundo abaixo para viver os seus dias, agora finitos, no corpo de Laila Starr, uma jovem de vinte e poucos anos que vive em Mumbai. Lutando com sua recém-descoberta mortalidade, Laila busca uma maneira de ser colocada no momento e no lugar onde o criador da imortalidade nascerá. Será que Laila aproveitará sua chance para impedir a Humanidade de alterar de forma permanente o ciclo da vida ou a morte realmente se tornará uma coisa do passado?",
@@ -22,6 +27,7 @@ const livros = [
     },
 
     {
+        id: '3',
         nome: "Os Devoradores da Noite: Ela Come a Noite",
         imagem: "https://i.postimg.cc/YCvBjkxm/image.jpg",
         descricao: "Gêmeos chineses americanos, Milly e Billy estão enfrentando momentos difíceis. Além dos vários fracassos em suas vidas pessoais e profissionais, eles estão lutando para manter o restaurante deles de pé. Felizmente, seus pais, Ipo e Keon, estão na cidade para a visita anual. Tendo imigrado de Hong Kong antes do nascimento dos gêmeos, Ipo e Keon têm apoiado seus filhos incondicionalmente, mas agora começam a se perguntar se esse apoio tornou Milly e Billy incapazes de se sustentarem.",
@@ -30,14 +36,32 @@ const livros = [
     }
 ]
 
-function mostraLivros (req, res) {
+//GET 
+function mostraLivros (res) {
     res.json(livros)
 }
 
+//POST
+function adicionaLivro (req, res) {
+    const livro = {
+        id: uuidv4(),
+        nome: req.body.nome,
+        imagem: req.body.imagem,
+        descricao: req.body.descricao,
+        citacao: req.body.citacao,
+        autor: req.body.autor
+    }
 
+    livros.push(livro);
+    res.json(livro);
+}
+
+// porta
 function mostraPorta () {
     console.log(`Servidor rodando na porta ${porta}`);
 }
 
-app.use(router.get('/livros', mostraLivros));
-app.listen(porta, mostraPorta);
+
+app.use(router.get('/livros', mostraLivros)); // Rota para mostrar os livros
+app.use(router.post('/livros', adicionaLivro)); // Rota para adicionar um livro
+app.listen(porta, mostraPorta); // Iniciando o servidor
